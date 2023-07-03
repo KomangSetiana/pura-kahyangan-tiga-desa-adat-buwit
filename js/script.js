0ac37c0ed3dd81f1b561c02da2f8f0d95cebf93f
I Komang Setiana <komangsetiana40@gmail.com>
2023-07-03 19:39:28 +0800

    initial commit

--- js/script.js
+++ js/script.js
@@ -16,11 +16,40 @@ document.addEventListener("click", function (e) {
   }
 });
 
-var swiper = new Swiper(".slide-content", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
+// Modal Box
+let itemDetailModal = document.querySelectorAll(".modal-open");
+
+itemDetailModal.forEach((btn) => {
+  btn.onclick = (e) => {
+    let modal = btn.getAttribute("data-modal");
+    document.getElementById(modal).style.display = "flex";
+    e.preventDefault();
+  };
 });
+
+// klik tombol close modal
+let closeBtns = document.querySelectorAll(".close-icon");
+closeBtns.forEach((btn) => {
+  btn.onclick = (e) => {
+    btn.closest(".modal").style.display = "none";
+    e.preventDefault();
+  };
+});
+
+// klik di luar modal
+window.onclick = (e) => {
+  if (e.target.className === "modal") {
+    e.target.style.display = "none";
+  }
+};
+
+// var id_aktif;x
+// function on(clicked_id) {
+//   document.getElementById("modal").style.display = "block";
+//   document.getElementById(clicked_id).style.display = "block";
+//   id_aktif = clicked_id;
+// }
+// function off() {
+//   document.getElementById("modal").style.display = "none";
+//   document.getElementById(id_aktif).style.display = "none";
+// }
